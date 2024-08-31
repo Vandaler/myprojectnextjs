c99921247f4daa8e9075a0f4b315e84354fb1077
panuwat thammabut <panuwat.tha@kkumail.com>
2024-08-31 13:45:44 +0700

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import  "bootstrap/dist/css/bootstrap.min.css"
 import styles from "./style.module.css";
 import Link from "next/link";
 
--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -4,7 +4,6 @@ import React, { useState , useEffect} from "react";
 import "./../../globals.css";
 import "./poke.css"
 import Image from "next/image";
-import  "bootstrap/dist/css/bootstrap.min.css"
 import EmojiComponent from "../emoji";
 
 interface PokemonDetails {
